0f8c241d509999b86c08ec5e91a780924cfa72ad
pyk <x24870@gmail.com>
2021-03-13 21:28:40 +0800

    main: print all bytes in the chuck

--- main.c
+++ main.c
@@ -2,6 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+size_t got_data(char *, size_t, size_t, void*);
+
 int main() {
     CURL *curl;
     FILE *fp;
@@ -11,7 +13,8 @@ int main() {
     curl = curl_easy_init();
 
     curl_easy_setopt(curl, CURLOPT_URL, "test.jigentec.com:49152");
-    curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    // curl_easy_setopt(curl, CURLOPT_WRITEDATA, fp);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, got_data);
     curl_easy_setopt(curl, CURLOPT_FAILONERROR, 1L);
 
     result = curl_easy_perform(curl);
@@ -24,4 +27,20 @@ int main() {
 
     fclose(fp);
     curl_easy_cleanup(curl);
+}
+
+size_t got_data(char *buffer, size_t itemsize, size_t nitems, void* ignored) {
+    size_t bytes = itemsize * nitems;
+    printf("New chunk %zu bytes\n", bytes);
+    int count = 16, linenum = 0;
+    int idx = 0;
+    while(idx < nitems){
+        if(count++ >= 16){
+            printf("\n%d: ", linenum++);
+            count = 0;
+        }
+        printf("%hhx ", buffer[idx++]);
+    }
+    printf("\n ---END--- \n");
+    return bytes;
 }
